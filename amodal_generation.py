00fb27ac5a02ea0258f3b9f7f5c13c9e8fd4ee81
pawakapan <pawakapan@pawakapans-MacBook-Air.local>
2018-10-15 22:40:20 -0400

    empty commit

--- amodal_generation.py
+++ amodal_generation.py
@@ -53,14 +53,14 @@ base_images = []
 base_masks = []
 base_subfolders = []
 base_image_ids = []
-for root,_,mask_paths in os.walk("gtFine_trainvaltest/gtFine"):
+for root,_,mask_paths in os.walk("gtFine"):
     for mask_path in mask_paths:
         if mask_path[-9:] == "color.png":
             total_count += 1
             subfolder = root[27:]
             image_id = mask_path[:-17]
-            base_masks.append(misc.imread("gtFine_trainvaltest/gtFine/" + subfolder + "/" + image_id+"_gtFine_color.png"))
-            base_images.append(misc.imread("leftImg8bit_trainvaltest/leftImg8bit/" + subfolder + "/" + image_id + "_leftImg8bit.png"))
+            base_masks.append(misc.imread("gtFine/" + subfolder + "/" + image_id+"_gtFine_color.png"))
+            base_images.append(misc.imread("leftImg8bit/" + subfolder + "/" + image_id + "_leftImg8bit.png"))
             base_subfolders.append(subfolder)
             base_image_ids.append(image_id)
             if len(images) % 1000 == 0:
