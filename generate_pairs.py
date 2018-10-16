6a06484f940bc63946e39442f56b04888231d766
A B <pp456@cornell.edu>
2018-10-16 04:07:01 -0400

    Update generate_pairs.py

--- generate_pairs.py
+++ generate_pairs.py
@@ -5,14 +5,82 @@ import matplotlib.pyplot as plt
 import os
 import json
 import cv2
+from itertools import product
+from scipy.ndimage.measurements import label
 
 f = open("amodal/dict_file.txt","r")
 
+car_color = [0,0,142,255]
+person_color = [255,0,0,255]
+bike_color = [119,11,32,255]
+ped_color = [220,20,60,255]
+pet_color = [111,74,0,255]
+bus_color = [0,60,100,255]
+truck_color = [0,0,90,255]
+trail_color = [0,0,110,255]
+motor_color = [0,0,230,255]
+license_color = [0,0,142,255]
+road_color = [128,64,128,255]
+
+def match_grid(mask,c):
+    return np.array((mask[:,:,0]==c[0])*(mask[:,:,1]==c[1])*(mask[:,:,2]==c[2])*(mask[:,:,3]==c[3]),dtype=np.float32)
+
 for i in range(227):
-    a = f.next()[:-1]
-    c1 = a.index(",")
-    c2 = a.index(",",c1+1)
-    subfolder = a[c1+1:c2]
-    im = a[c2+1:]
-    mask_file = "gtFine_trainvaltest/gtFine" + subfolder + "/" + im + "_gtFine_color.png"
-    img_file = "leftImg8bit_trainvaltest/leftImg8bit" + subfolder + "/" + im + "_leftImg8bit.png"
+    match_scores = []
+    paths = []
+    base_image = misc.imread("base/" + str(i) + "_image.png")
+    base_mask = misc.imread("base/" + str(i) + "_mask.png")
+    base_road = match_grid(base_mask,road_color)
+    for root,_,mask_paths in os.walk("gtFine_trainvaltest/gtFine"):
+        if not ("/test" in root):
+            for mask_path in mask_paths:
+                if mask_path[-9:] == "color.png":
+                    subfolder = root[6:]
+                    image_id = mask_path[:-17]
+                    mask = misc.imread("gtFine_trainvaltest/gtFine/" + subfolder + "/" + image_id+"_gtFine_color.png")
+                    paths.append(root+"/"+mask_path)
+                    road = match_grid(mask,road_color)
+                    match = np.where(road>base_road,-1.0,1.0)*road
+                    score = np.sum(match)/np.sum(road)
+                    match_scores.append(score)
+    order = np.argsort(match_scores)[:20]
+    
+    total_copies = 0
+    for idx in order:
+        mask = misc.imread(paths[idx])
+        cargrid = match_grid(mask,car_color)
+        pergrid = match_grid(mask,person_color)
+        bikegrid = match_grid(mask,bike_color)
+        pedgrid = match_grid(mask,ped_color)
+        petgrid = match_grid(mask,pet_color)
+        truckgrid = match_grid(mask,truck_color)
+        trailgrid = match_grid(mask,trail_color)
+        motorgrid = match_grid(mask,motor_color)
+        licensegrid = match_grid(mask,license_color)
+        roadgrid = match_grid(mask,road_color)
+        fore_grid = np.minimum(cargrid+pergrid+bikegrid+pedgrid+petgrid+truckgrid+trailgrid+motorgrid+licensegrid,1.0)
+        labeled_array,num_features = label(fore_grid,structure=np.ones([3,3]))
+        
+        total_features = 0
+        new_base = np.copy(base_image)
+        width = len(mask)
+        height = len(mask[0])
+        coordinates = list(product(xrange(width), xrange(height)))
+        coordinates = np.reshape(coordinates,[width,height,2])
+        for i in range(1,num_features+1):
+            grid = np.where(labeled_array==i,1.0,0.0)
+            grid_3d = np.tile(np.reshape(grid,[1024,2048,1]),(1,1,3))
+            c = np.reshape(grid,[len(mask),len(mask[0]),1])*coordinates
+            ground_y = np.max(c[:,:,0])
+            ground_x = np.sum(c[:,:,1])/np.sum(grid)
+            if np.sum(base_mask[int(ground_y)][int(ground_x)]-np.array(road_color)) == 0:
+                new_base = np.where(grid_3d==1,image,new_base)
+                total_features += 0
+        if total_features > 0:
+            misc.imsave(str(i) + "_" + str(total_copies) + ".png")
+            total_copies += 1
+        
+        
+
+
+
