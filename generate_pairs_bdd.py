295d715ab23d36d2c1ba25832baebe8ce9e84eb1
A B <pp456@cornell.edu>
2018-10-20 16:28:34 -0400

    Update generate_pairs_bdd.py

--- generate_pairs_bdd.py
+++ generate_pairs_bdd.py
@@ -91,7 +91,7 @@ for i in range(219):
     match_scores.append(match_scores_)
     order = np.argsort(match_scores,axis=0)
     order = np.argsort(match_scores,axis=0)
-    order = order[:,20]
+    order = order[:,:20]
     np.save("order_bdd.npy",order)
     match_file.write(str(i)+"\n")
     for j in order[i]:
