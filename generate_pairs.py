5e7bd254b32f14fa16aec22ea974015dcaa1eecc
Pian Pawakapan <pp456@cornell.edu>
2018-11-07 15:03:06 -0500

    Update generate_pairs.py

--- generate_pairs.py
+++ generate_pairs.py
@@ -83,7 +83,7 @@ for i in range(227):
     for root,_,mask_paths in os.walk("cityscapes/gtFine"):
         if not ("/test" in root):
             for mask_path in mask_paths:
-                if mask_path[-9:] == "color.png" and (("val" in base_path and "val" in mask_path) or ("train" in base_path and "train" in mask_path)):
+                if mask_path[-9:] == "color.png" and (("val" in base_path and "val" in root) or ("train" in base_path and "train" in root)):
                     subfolder = root[6:]
                     image_id = mask_path[:-17]
                     mask = misc.imread("cityscapes/gtFine/" + subfolder + "/" + image_id+"_gtFine_color.png")
