06912ca91860ecb4c09c7ee512686f13983f5a17
Pian Pawakapan <pp456@cornell.edu>
2018-11-08 11:46:30 -0500

    Update generate_pairs.py

--- generate_pairs.py
+++ generate_pairs.py
@@ -63,6 +63,7 @@ for i in range(227):
     base_image = misc.imread("cityscapes/gtFine" + rtpth + "_gtFine_color.png")
     base_mask = misc.imread("cityscapes/leftImg8bit" + rtpth + "_leftImg8bit.png")
     base_paths.append(rtpth)
+    print(rtpth)
     
     base_images.append(base_image)
     base_masks.append(base_mask)
@@ -92,30 +93,38 @@ for i in range(227):
     for root,_,mask_paths in os.walk("cityscapes/gtFine"):
         if not ("/test" in root):
             for mask_path in mask_paths:
-                if mask_path[-9:] == "color.png" and (("val" in base_path and "val" in root) or ("train" in base_path and "train" in root)):
-                    subfolder = root[17:]
-                    image_id = mask_path[:-17]
-                    mask = misc.imread("cityscapes/gtFine/" + subfolder + "/" + image_id+"_gtFine_color.png")
-                    paths.append((subfolder,image_id))
-                    
-                    road = match_grid(mask,road_color)
-                    ground = match_grid(mask,ground_color)
-                    side = match_grid(mask,side_color)
-                    veg = match_grid(mask,veg_color)
-                    terrain = match_grid(mask,terrain_color)
-                    
-                    match_road = np.where(road>base_road,1.0,-1.0)*road
-                    match_side = np.where(side>base_side,1.0,-1.0)*side
-                    match_ground = np.where(ground>base_ground,1.0,-1.0)*ground
-                    match_veg = np.where(veg>base_veg,1.0,-1.0)*veg
-                    match_terrain = np.where(terrain>base_terrain,1.0,-1.0)*terrain
-                    if np.sum(road+side+ground+veg+terrain) == 0:
-                        score = np.inf
+                if mask_path[-9:] == "color.png" 
+                    if (("val" in base_path and "val" in root) or ("train" in base_path and "train" in root)):
+                        subfolder = root[17:]
+                        image_id = mask_path[:-17]
+                        mask = misc.imread("cityscapes/gtFine/" + subfolder + "/" + image_id+"_gtFine_color.png")
+                        paths.append((subfolder,image_id))
+
+                        road = match_grid(mask,road_color)
+                        ground = match_grid(mask,ground_color)
+                        side = match_grid(mask,side_color)
+                        veg = match_grid(mask,veg_color)
+                        terrain = match_grid(mask,terrain_color)
+
+                        match_road = np.where(road>base_road,1.0,-1.0)*road
+                        match_side = np.where(side>base_side,1.0,-1.0)*side
+                        match_ground = np.where(ground>base_ground,1.0,-1.0)*ground
+                        match_veg = np.where(veg>base_veg,1.0,-1.0)*veg
+                        match_terrain = np.where(terrain>base_terrain,1.0,-1.0)*terrain
+                        if np.sum(road+side+ground+veg+terrain) == 0:
+                            score = np.inf
+                        else:
+                            score = np.sum(match_road+match_side+match_ground+match_veg+match_terrain)/np.sum(road+side+ground+veg+terrain)
+                        match_scores_.append(score)
                     else:
-                        score = np.sum(match_road+match_side+match_ground+match_veg+match_terrain)/np.sum(road+side+ground+veg+terrain)
-                    match_scores_.append(score)
+                        match_scores_.append(np.inf)
                     
+    print(order)
+    print(len(order))
     order = np.argsort(match_scores_)[:20]
+    print(order)
+    print(len(order))
+    print(len(paths))
     match_file.write(str(i)+"\n")
     for j in order:
         match_file.write(str(paths[j])+"\n")
