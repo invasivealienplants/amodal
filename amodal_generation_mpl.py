856571329cfbb197da1ccd1315af205f4f63ca3e
Pian Pawakapan <pp456@cornell.edu>
2018-11-28 19:42:31 -0500

    Update amodal_generation_mpl.py

--- amodal_generation_mpl.py
+++ amodal_generation_mpl.py
@@ -41,43 +41,37 @@ sky_color = [70,130,180]
 water_color = [0,170,30]
 terrain_color = [152,251,152]
 
-def calculate_foreground(images,masks):
-    # foreground
-    percent_foreground = []
-    for i,(image,mask) in enumerate(zip(images,masks)):
-        image = np.array(image)
-        mask = np.array(mask)
+def calculate_foreground(image,mask):
         
-        trailgrid = match_grid(mask,trail_color)
-        ganimalgrid = match_grid(mask,ganimal_color)
-        busgrid = match_grid(mask,bus_color)
-        bikegrid = match_grid(mask,bike_color)
-        vehicgrid = match_grid(mask,vehic_color)
-        ridergrid = match_grid(mask,rider_color)
-        motorgrid = match_grid(mask,motor_color)
-        motorcgrid = match_grid(mask,motorc_color)
-        pergrid = match_grid(mask,per_color)
-        egogrid = match_grid(mask,ego_color)
-        cargrid = match_grid(mask,car_color)
-        bicygrid = match_grid(mask,bicy_color)
-        carvgrid = match_grid(mask,carv_color)
-        truckgrid = match_grid(mask,truck_color)
-        whslowgrid = match_grid(mask,whslow_color)
-        
-        manholegrid = match_grid(mask,manhole_color)
-        lanemarkgrid = match_grid(mask,lanemark_color)
-        crosswalkgrid = match_grid(mask,crosswalk_color)
-        cwalkpgrid = match_grid(mask,cwalkp_color)
-        roadgrid = match_grid(mask,road_color)
-        
-        foregrid = np.minimum(1.0,trailgrid+ganimalgrid+busgrid+bikegrid+vehicgrid+ridergrid+motorgrid+motorcgrid+pergrid+egogrid+cargrid+bicygrid+carvgrid+truckgrid+whslowgrid)
-        roadgrid = np.minimum(1.0,manholegrid+lanemarkgrid+crosswalkgrid+cwalkpgrid+roadgrid)
-        
-        if np.mean(roadgrid) < 0.05:
-            percent_foreground.append(np.inf)
-        else:
-            percent_foreground.append(np.mean(foregrid))
-    return percent_foreground
+    trailgrid = match_grid(mask,trail_color)
+    ganimalgrid = match_grid(mask,ganimal_color)
+    busgrid = match_grid(mask,bus_color)
+    bikegrid = match_grid(mask,bike_color)
+    vehicgrid = match_grid(mask,vehic_color)
+    ridergrid = match_grid(mask,rider_color)
+    motorgrid = match_grid(mask,motor_color)
+    motorcgrid = match_grid(mask,motorc_color)
+    pergrid = match_grid(mask,per_color)
+    egogrid = match_grid(mask,ego_color)
+    cargrid = match_grid(mask,car_color)
+    bicygrid = match_grid(mask,bicy_color)
+    carvgrid = match_grid(mask,carv_color)
+    truckgrid = match_grid(mask,truck_color)
+    whslowgrid = match_grid(mask,whslow_color)
+
+    manholegrid = match_grid(mask,manhole_color)
+    lanemarkgrid = match_grid(mask,lanemark_color)
+    crosswalkgrid = match_grid(mask,crosswalk_color)
+    cwalkpgrid = match_grid(mask,cwalkp_color)
+    roadgrid = match_grid(mask,road_color)
+
+    foregrid = np.minimum(1.0,trailgrid+ganimalgrid+busgrid+bikegrid+vehicgrid+ridergrid+motorgrid+motorcgrid+pergrid+egogrid+cargrid+bicygrid+carvgrid+truckgrid+whslowgrid)
+    roadgrid = np.minimum(1.0,manholegrid+lanemarkgrid+crosswalkgrid+cwalkpgrid+roadgrid)
+
+    if np.mean(roadgrid) < 0.05:
+        return np.inf
+    else:
+        return np.mean(foregrid)
   
 def get_top_k(images,masks,subfolders,image_ids,k=250):
     percent_foreground = calculate_foreground(images,masks)
@@ -94,8 +88,7 @@ def get_top_k(images,masks,subfolders,image_ids,k=250):
     return i_,m_,s_,id_
 
 total_count = 0
-base_images = []
-base_masks = []
+base_scores = []
 base_subfolders = []
 base_image_ids = []
 for root,_,mask_paths in os.walk("mapillary/data"):
@@ -105,17 +98,34 @@ for root,_,mask_paths in os.walk("mapillary/data"):
             total_count += 1
             subfolder = root.split("mapillary/data/")[-1]
             image_id = mask_path.split(".jpg")[0]
-            base_images.append(misc.imread(root+"/"+mask_path))
+            image = misc.imread(root+"/"+mask_path)
             if "training" in subfolder:
-                base_masks.append(misc.imread("mapillary/data/training/labels/"+image_id+".png"))
+                mask = misc.imread("mapillary/data/training/labels/"+image_id+".png")
             else:
-                base_masks.append(misc.imread("mapillary/data/validation/labels/"+image_id+".png"))
+                mask = misc.imread("mapillary/data/validation/labels/"+image_id+".png")
             base_subfolders.append(subfolder)
             base_image_ids.append(image_id)
-            if len(base_images) % 500 == 0 and total_count > 0:
+            base_scores.append(calculate_foreground(image,mask))
+            if len(base_images) % 1500 == 0 and total_count > 0:
                 print("Images passed through : " + str(total_count))
-                base_images,base_masks,base_subfolders,base_image_ids = get_top_k(base_images,base_masks,base_subfolders,base_image_ids,k=400)
-base_images,base_masks,base_subfolders,base_image_ids = get_top_k(base_images,base_masks,base_subfolders,base_image_ids,k=400)
+                order = np.argsort(base_scores)[:750]
+                new_scores = []
+                new_sf = []
+                new_ids = []
+                for idx in order:
+                    new_scores.append(base_scores[idx])
+                    new_sf.append(base_subfolder[idx])
+                    new_ids.append(base_image_ids[idx])
+                base_scores,base_subfolders,base_image_ids = new_scores,new_sf,new_ids
+order = np.argsort(base_scores)[:750]
+new_scores = []
+new_sf = []
+new_ids = []
+for idx in order:
+    new_scores.append(base_scores[idx])
+    new_sf.append(base_subfolder[idx])
+    new_ids.append(base_image_ids[idx])
+base_scores,base_subfolders,base_image_ids = new_scores,new_sf,new_ids
 
 # save images
 for i,(base_image,base_mask,base_subfolder,base_id) in enumerate(zip(base_images,base_masks,base_subfolders,base_image_ids)):
